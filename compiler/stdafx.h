57d15d35346fd8d6ab032bdc2bb6837ddf165701
Park June Chul <pjc0247@naver.com>
2018-10-16 22:31:19 +0900

    sfdafx.h 정리

--- compiler/stdafx.h
+++ compiler/stdafx.h
@@ -12,22 +12,27 @@
 
 #define rklog(fmt, ...) do { if (DEBUG) printf(fmt, __VA_ARGS__); } while (0)
 
-#if WIN32
-#define stdinvoke std::invoke
-#define CONSTEXPR constexpr
-#define THREAD_LOCAL thread_local
+#ifdef _MSC_VER
+    #define stdinvoke std::invoke
 #else
-#define __forceinline inline
-#define stdinvoke std::__invoke
-#define CONSTEXPR
-#define THREAD_LOCAL
+    #define __forceinline inline
+    #define stdinvoke std::__invoke
 #endif
-
 
 #ifdef __EMSCRIPTEN__
     #define RK_ENV_WEB
+    #define RK_HALT_ON_LONG_EXECUTION
 #else
     #define RK_ENV_NATIVE
+#endif
+
+/* Some specifiers cannot be used in em++. */
+#ifdef RK_ENV_WEB
+    #define CONSTEXPR
+    #define THREAD_LOCAL
+#else
+    #define CONSTEXPR constexpr
+    #define THREAD_LOCAL thread_local
 #endif
 
 std::wstring str2wstr(const char* text);
