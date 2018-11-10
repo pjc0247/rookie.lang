6ccfe1da7d6d9d6c766a7b18a371520cefd86547
Park June Chul <pjc0247@naver.com>
2018-11-11 00:51:09 +0900

    [WIP] Refactoring

---
+++ libs/dictionary.cpp
@@ -0,0 +1,99 @@
+#include "stdafx.h"
+
+#include <algorithm>
+
+#include "string.h"
+#include "object.h"
+#include "iterator.h"
+
+#include "dictionary.h"
+
+void rkdictionary::import(binding &b) {
+    auto type = type_builder(L"dictionary");
+
+    type.method(rk_id_new, create_array);
+
+    method(type, rk_id_getitem, &rkdictionary::get);
+    method(type, rk_id_setitem, &rkdictionary::set);
+    method(type, rk_id_tostring, &rkdictionary::to_string);
+    method(type, L"size", &rkdictionary::size);
+    method(type, L"contains", &rkdictionary::contains);
+    method(type, L"clear", &rkdictionary::clear);
+    method(type, L"remove", &rkdictionary::remove);
+
+    method(type, L"get_iterator", &rkdictionary::get_iterator);
+
+    b.add_type(type);
+}
+
+rkdictionary::rkdictionary(int n) {
+    for (int i = 0; i < n; i++) {
+        value v = rkctx()->next_param();
+        value k = rkctx()->next_param();
+        set(k, v);
+    }
+}
+
+value rkdictionary::create_dictionary(value_cref idx) {
+    return value::mkobjref(new rkdictionary(0));
+}
+
+value rkdictionary::get(value_cref key) {
+    auto h = rkwstr(key);
+    return dic[h];
+}
+value rkdictionary::set(value_cref key, value_cref v) {
+    auto h = rkwstr(key);
+    dic[h] = v;
+    return rknull;
+}
+value rkdictionary::to_string() {
+    std::wstring str;
+
+    str += L"{";
+    for (auto &v : dic) {
+        auto &key = v.first;
+        auto &value = v.second;
+
+        if (v != *dic.begin())
+            str += L", ";
+
+        str += key + L" : ";
+        str += rk_call_tostring(value);
+    }
+    str += L"}";
+
+    return str2rk(str);
+}
+
+value rkdictionary::size() {
+    return value::mkinteger(dic.size());
+}
+value rkdictionary::contains(value_cref key) {
+    auto h = rkwstr(key);
+    if (dic.find(h) == dic.end())
+        return value::_false();
+    return value::_true();
+}
+value rkdictionary::clear() {
+    dic.clear();
+    return rknull;
+}
+value rkdictionary::remove(value_cref key) {
+    auto h = rkwstr(key);
+    if (dic.erase(h))
+        return value::_true();
+    return value::_false();
+}
+
+value rkdictionary::get_iterator() {
+    auto it = new rkdictionary_iterator(dic);
+    return obj2rk(it);
+}
+
+std::map<std::wstring, value>::iterator rkdictionary::begin() {
+    return dic.begin();
+}
+std::map<std::wstring, value>::iterator rkdictionary::end() {
+    return dic.end();
+}
--- libs/dictionary.h
+++ libs/dictionary.h
@@ -1,108 +1,33 @@
 #pragma once
 
-#include <algorithm>
 #include <string>
 #include <map>
 
 #include "binding.h"
-#include "string.h"
-#include "object.h"
-
-#include "iterator.h"
 
 class rkdictionary : public rkobject<rkdictionary> {
 public:
     TYPENAME(L"dictionary")
 
-    static void import(binding &b) {
-        auto type = type_builder(L"dictionary");
+    static void import(binding &b);
 
-        type.method(rk_id_new, create_array);
+    rkdictionary(int n);
 
-        method(type, rk_id_getitem, &rkdictionary::get);
-        method(type, rk_id_setitem, &rkdictionary::set);
-        method(type, rk_id_tostring, &rkdictionary::to_string);
-        method(type, L"size", &rkdictionary::size);
-        method(type, L"contains", &rkdictionary::contains);
-        method(type, L"clear", &rkdictionary::clear);
-        method(type, L"remove", &rkdictionary::remove);
+    static value create_dictionary(value_cref idx);
 
-        method(type, L"get_iterator", &rkdictionary::get_iterator);
-
-        b.add_type(type);
-    }
-
-    rkdictionary(int n) {
-        for (int i = 0; i < n; i++) {
-            value v = rkctx()->next_param();
-            value k = rkctx()->next_param();
-            set(k, v);
-        }
-    }
-
-    value static create_array(value_cref idx) {
-        return value::mkobjref(new rkdictionary(0));
-    }
-
-    value get(value_cref key) {
-        auto h = rkwstr(key);
-        return dic[h];
-    }
-    value set(value_cref key, value_cref v) {
-        auto h = rkwstr(key);
-        dic[h] = v;
-        return rknull;
-    }
-    value to_string() {
-        std::wstring str;
-
-        str += L"{";
-        for (auto &v : dic) {
-            auto &key = v.first;
-            auto &value = v.second;
-
-            if (v != *dic.begin())
-                str += L", ";
-
-            str += key + L" : ";
-            str += rk_call_tostring(value);
-        }
-        str += L"}";
-
-        return str2rk(str);
-    }
+    value get(value_cref key);
+    value set(value_cref key, value_cref v);
+    value to_string();
     
-    value size() {
-        return value::mkinteger(dic.size());
-    }
-    value contains(value_cref key) {
-        auto h = rkwstr(key);
-        if (dic.find(h) == dic.end())
-            return value::_false();
-        return value::_true();
-    }
-    value clear() {
-        dic.clear();
-        return rknull;
-    }
-    value remove(value_cref key) {
-        auto h = rkwstr(key);
-        if (dic.erase(h))
-            return value::_true();
-        return value::_false();
-    }
+    value size();
+    value contains(value_cref key);
+    value clear();
+    value remove(value_cref key);
 
-    value get_iterator() {
-        auto it = new rkdictionary_iterator(dic);
-        return obj2rk(it);
-    }
+    value get_iterator();
 
-    std::map<std::wstring, value>::iterator begin() {
-        return dic.begin();
-    }
-    std::map<std::wstring, value>::iterator end() {
-        return dic.end();
-    }
+    std::map<std::wstring, value>::iterator begin();
+    std::map<std::wstring, value>::iterator end();
 
 private:
     std::map<std::wstring, value> dic;
